caf4eb6cf49fe9ee2a0fb61b9345a8a41f83cc7f
Arvind Sharma <41388904+ArvindSharma18@users.noreply.github.com>
2020-12-15 19:54:38 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -144,9 +144,3 @@ MESSAGE_TAGS = {
     50: 'critical',
 }
 
-'''Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'karvind199886@gmail.com'
-EMAIL_HOST_PASSWORD = 'hiiamarvindcool'
-EMAIL_USE_TLS = True'''
